b1d95527abc105b09f7f615fe7c6660695cfc58a
FENIXSHIRO <112804060+FENIXSHIRO@users.noreply.github.com>
2025-10-07 17:23:57 +0300

    feat: получение пути

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import StatusIcon from './components/StatusIcon.vue'
+import { ref } from 'vue'
+
+const path = ref<string>()
+
+const selectFolder = async (): Promise<void> => {
+  const selectedPath = await window.electronAPI.selectFolder()
+  if (selectedPath) path.value = selectedPath
+}
 </script>
 
 <template>
   <div class="">
-    <StatusIcon status="needUpdate" />
+    <StatusIcon status="loading" />
+
+    <div class="">
+      <button type="button" @click="selectFolder">Указать папку</button>
+    </div>
+
+    <div class="">
+      {{ path }}
+    </div>
   </div>
 </template>
